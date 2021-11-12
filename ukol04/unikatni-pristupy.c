38566168a6b9e5c82740a3d4d18013e190f61c8c
Jan Černý <cernyj87@fit.cvut.cz>
2021-11-12 16:12:15 +0100

    hw04: Rewrite, new algorithm using chunks

--- ukol04/unikatni-pristupy.c
+++ ukol04/unikatni-pristupy.c
@@ -5,12 +5,19 @@
 #define ACCESS_MAX 1000000
 #define ID_MAX 99999
 
+const int CHUNK_SIZE = 100000;
+
 int accesCounts[ID_MAX + 1];
 
 short hasSeenId[ID_MAX + 1];
+short hasSeenInChunk[ID_MAX + 1];
+int valueIndex = 0;
 
 int accessLog[ACCESS_MAX];
 int lastLogIndex = 0;
+
+int chunks[10][100000];
+int chunkIndex = 0;
 
 void getQuery(int from, int to)
 {
@@ -19,10 +26,26 @@ void getQuery(int from, int to)
     int distinctCount = 0;
     for (int i = from; i <= to; i++)
     {
-        if (hasSeenId[accessLog[i]] == 0)
+        if (i % CHUNK_SIZE == 0 && i + CHUNK_SIZE <= to)
         {
-            hasSeenId[accessLog[i]] = 1;
-            distinctCount++;
+            int chunkIndex = i / CHUNK_SIZE;
+            for (int valueIndex = 0; chunks[chunkIndex][valueIndex] != -1 && valueIndex < CHUNK_SIZE; valueIndex++)
+            {
+                if (hasSeenId[chunks[chunkIndex][valueIndex]] == 0)
+                {
+                    hasSeenId[chunks[chunkIndex][valueIndex]] = 1;
+                    distinctCount++;
+                }
+            }
+            i += CHUNK_SIZE - 1;
+        }
+        else
+        {
+            if (hasSeenId[accessLog[i]] == 0)
+            {
+                hasSeenId[accessLog[i]] = 1;
+                distinctCount++;
+            }
         }
     }
 
@@ -38,11 +61,30 @@ void addAccess(int userId)
     else
         printf("> navsteva #%d\n", accesCounts[userId]);
 
-    accessLog[lastLogIndex++] = userId;
+    accessLog[lastLogIndex] = userId;
+
+    if (hasSeenInChunk[userId] == 0)
+    {
+        hasSeenInChunk[userId] = 1;
+        chunks[chunkIndex][valueIndex] = userId;
+        valueIndex++;
+    }
+
+    if ((lastLogIndex + 1) % CHUNK_SIZE == 0)
+    {
+        memset(hasSeenInChunk, 0, sizeof(hasSeenInChunk));
+        chunkIndex++;
+        valueIndex = 0;
+    }
+
+    lastLogIndex++;
 }
 
 int main(int argc, char const *argv[])
 {
+
+    memset(chunks, -1, sizeof(chunks));
+
     printf("Pozadavky:\n");
 
     char symbol = 0;
