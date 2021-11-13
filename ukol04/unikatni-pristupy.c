71fc53853c6299d2a92974b2aa067a63236a4882
Jan Černý <cernyj87@fit.cvut.cz>
2021-11-13 21:36:25 +0100

    hw04: Add comments

--- ukol04/unikatni-pristupy.c
+++ ukol04/unikatni-pristupy.c
@@ -1,3 +1,11 @@
+/**
+ * @file unikatni-pristupy.c
+ * @author Jan Cerny (cernyj87@fit.cvut.cz)
+ * @brief Program to count how many unique accesses were logged in a specified range
+ * @version 1.0
+ * @date 2021-11-13
+ * 
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,15 +13,27 @@
 #define ACCESS_MAX 1000000
 #define ID_MAX 99999
 
+// Store the count of accesses for provided ID (ID is used as an index)
 int accesCounts[ID_MAX + 1];
+
+// Log ID of the access
 int accessLog[ACCESS_MAX];
 
+// Store 0 if not seen, 1 if seen (ID is used as an index)
 short hasSeenId[ID_MAX + 1];
 
+/**
+ * @brief Query the log for unique accesses in provided range
+ * 
+ * @param from Start of the range
+ * @param to End of the range
+ */
 void getQuery(int from, int to)
 {
+    // Count numbers in range
     int count = to - from + 1;
 
+    // Count uniques numbers in range
     int distinctCount = 0;
     for (int i = from; i <= to; i++)
     {
@@ -24,6 +44,7 @@ void getQuery(int from, int to)
         }
     }
 
+    // Reset the global array for another query
     memset(hasSeenId, 0, sizeof(hasSeenId));
 
     printf("> %d / %d\n", distinctCount, count);
@@ -36,6 +57,7 @@ void addAccess(int userId, int *lastLogIndex)
     else
         printf("> navsteva #%d\n", accesCounts[userId]);
 
+    // Add the access into an array and increment the index for next adding
     accessLog[(*lastLogIndex)++] = userId;
 }
 
